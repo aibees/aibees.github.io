f1f034f70c90c7e9e1a807d491372ae5c129c3f4
aibees <aibees1129@naver.com>
2021-02-08 01:25:41 +0900

    log update

---
+++ scripts/default.js
@@ -0,0 +1,35 @@
+var access = function(menuid, pwd) {
+    window.open('./login', '_blank', 'width=400px,height=280px,top=350px,left=800px,scrollbars=no,location=no');
+}
+
+var articleAccess = function(title) {
+    var now = new Date();
+    var data = JSON.stringify({
+        title: title,
+        ip : ip()
+    })
+    sendAjax('POST', 'http:101.101.219.206/blog/postlog', data);
+}
+/**********************
+***  ajax function  ***
+***********************/
+
+var sendAjax = function(type, url, data) {
+    $.ajax({
+        type: type, // get / post / put
+        url: url,
+        async: true,
+        headers : {
+            "Content-Type" : "application/json"
+        },
+        dataType: 'json',
+        data: data,
+
+        success : function(ret) {
+            
+        },
+        error : function(request, status, error) {
+            alert(error)
+        }
+    })
+}
